afd19a854aa8b4002c2e09f9b4b0586ff836a6b9
arima <936446507@qq.com>
2019-05-15 23:54:00 +0800

    二分查找

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -52,4 +52,37 @@ function longestCommonPrefix(strs) {
   return findLongestCommonPrefix(strs, 0, strs.length - 1)
 }
 
+// 二分查找
+const longestCommonPrefix = (strs) => {
+  if (!strs) return '';
+
+  // 数组长度小于2，直接返回空
+  const length = strs.length;
+  if (length < 2) return strs[0] || '';
+
+  // 第一个元素不存在，直接返回空
+  let firstStr = strs[0];
+
+  // 递归进行二分法查找
+  const loop = (start, end, str) => {
+    // 下标超出范围, 结束并返回空字符串
+    if (start > end) return '';
+
+    // 取中间下标, >> 右移一位相当于除2, 右移n位相当于除以2的n次方。
+    const middle = start === end ? start : start + ((end - start) >> 1);
+
+    // 截取字符，循环遍历是否存在于所有元素，根据情况进行下一轮二分查找
+    let s = str.slice(0, middle);
+    for (let i = 1; i < length; i += 1) {
+      // 有空元素直接返回空
+      if (!strs[i]) return '';
+      if (strs[i].slice(0, middle) !== s) return loop(start, middle - 1, s);
+    }
+    return loop(middle + 1, end, str) || s;
+  };
+
+  // loop(startIndex + 1， endIndex + 1, str)
+  return loop(1, firstStr.length, firstStr);
+};
+
 longestCommonPrefix(["flower", "flow", "flight", 'flww'])
