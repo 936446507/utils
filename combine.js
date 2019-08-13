51e8c0161ac35d743482a7a42361e577a6538272
hhx <936446507@qq.com>
2019-08-13 18:54:09 +0800

    组合

---
+++ combine.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+  const result = [];
+  if (n < k) return result;
+
+  const end = n.length - k;
+  let curIndex = 0;
+  for (let i = curIndex; i < end; i++) {
+
+  }
+};
