0af416826798e08f83c36cdeda60b56b4da856a0
YuuTa <936446507@qq.com>
2019-10-18 00:34:48 +0800

    hasGroupsSizeX

--- hasGroupsSizeX.js
+++ hasGroupsSizeX.js
@@ -6,9 +6,20 @@ function gcd(a, b) {
   const diff = Math.abs(a - b);
   return b === diff ? b : gcd(b, diff);
 }
-var a = gcd(98, 63)
+
 var hasGroupsSizeX = function(deck) {
   if (deck.length < 2) return false;
+  const counts = {}
+  for (let item of deck) {
+    counts[item] = counts[item] ? counts[item] + 1 : 1;
+  }
+
+  let g = -1;
+  for (let key in counts) {
+    g = g === -1 ? counts[key] : gcd(g, counts[key]);
+  }
+
+  return g >= 2;
 };
 
 hasGroupsSizeX([1,1,1,1,2,2,2,2,2,2])
